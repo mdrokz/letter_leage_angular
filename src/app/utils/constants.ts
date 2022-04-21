c8b582f2befa08bdd75deabfdffb5065f8c6d6d5
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-04-21 06:01:18 +0530

    implement letter bag shuffling

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,33 +1,39 @@
 type LetterBag = String[];
 
+interface Alea {
+    (seed: string): () => number;
+}
+
+declare const alea: Alea;
+
 declare global {
     interface Array<T> {
-        shuffle: (take: number) => LetterBag
+        shuffle: () => LetterBag
         generate: (length: number) => LetterBag
     }
+
+    // interface Math {
+    //     seedrandom: (seed: string, options?: SeedRandom) => () => number;
+    // }
+}
+
+const LETTERS: LetterBag = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "O", "M", "N", "P", "Q", "R", "S", "T", "U", "W", "W", "X", "Y", "Z"];
+
+
+// const shuffler = Math.seedrandom(LETTERS.join(""), { entropy: true });
+// const shuffler = alea(LETTERS.join(""));
+
+const range = (min: number, max: number, magic: number) => Math.floor(Math.random() * (max - (magic - min)) + min)
+
+Array.prototype.generate = (length: number) => {
+    return LETTERS.concat(new Array<string>(length - LETTERS.length).fill("").map(_ => LETTERS[Math.floor(Math.random() * 26)])).shuffle();
 }
 
 
-Array.prototype.generate = function(length: number) {
-    return new Array<string>(length).fill("");
+Array.prototype.shuffle = function () {
+
+    return this.reverse().sort((a: string, b: string) => range(26, this.length, a.charCodeAt(0)) - range(26, this.length, b.charCodeAt(0)));
 }
-
-
-Array.prototype.shuffle = function(take: number) {
-    
-    return this;
-}
-
-const LETTERS: LetterBag = ["A","B","C","D","E","F","G","H","I","J","K","L","O","M","N","P","Q","R","S","T","U","W","W","X","Y","Z"];
-
 
 
 export default LETTERS;
-
-function makeid() {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var charactersLength = 1;
-      result = characters.charAt(Math.floor(Math.random() * charactersLength));
-   return result;
-}
