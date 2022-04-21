297cfac1e46f7c65799511730aa945ed0b8a8abe
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-04-21 06:00:39 +0530

    add modules & components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BagComponent } from './bag/bag.component';
+import {MatCardModule} from '@angular/material/card';
+import { LetterTrayComponent } from './letter-tray/letter-tray.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoardComponent
+    BoardComponent,
+    BagComponent,
+    LetterTrayComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
